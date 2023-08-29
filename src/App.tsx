0f6d62d0ee4b4083f55a929b2d42a05f792806b8
sujikim <sujikim@student.42seoul.kr>
2023-08-30 01:46:26 +0900

    feat: 기본 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Layout from 'components/commons/Layout';
+import IssueList from 'pages/IssueList';
+import IssueDetail from 'pages/IssueDetail';
+import Error from 'pages/Error';
+import NotFound from 'pages/NotFound';
+
 const App = () => {
-  return <div className="App">app</div>;
+  const router = createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route index element={<IssueList />} errorElement={<Error />} />
+      <Route path=":id" element={<IssueDetail />} errorElement={<NotFound />} />
+    </Route>,
+  );
+  return <RouterProvider router={createBrowserRouter(router)} />;
 };
 
 export default App;
