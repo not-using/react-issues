e00aaa8db51da41f17ac9b3829bb129b11315d96
sujikim <sujikim@student.42seoul.kr>
2023-08-30 08:47:36 +0900

    feat: 이슈 본문에 해당 이슈 링크 추가

--- src/pages/IssueDetail.tsx
+++ src/pages/IssueDetail.tsx
@@ -1,12 +1,14 @@
-import { useRouteLoaderData } from 'react-router-dom';
+import { useRouteLoaderData, Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 import type { IssueDetail } from 'types/issue';
 import IssueBody from 'components/issue/IssueBody';
 import IssueInfo from 'components/issue/IssueInfo';
 import UserAvatar from 'components/issue/UserAvatar';
+import { OWNER, REPOSITORY } from 'configs/octokit';
 
 const IssueDetailComponent = () => {
   const issueWithBody = useRouteLoaderData('number') as IssueDetail;
+  const issueLink = `https://github.com/${OWNER}/${REPOSITORY}/issues/${issueWithBody.number}`;
 
   return (
     <>
@@ -14,6 +16,7 @@ const IssueDetailComponent = () => {
         <UserAvatar user={issueWithBody.user} />
         <IssueInfo issue={issueWithBody} />
       </StyledDiv>
+      <Link to={issueLink}>Go to Issue</Link>
       <IssueBody body={issueWithBody.body} />
     </>
   );
