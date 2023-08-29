3a534ed7b15130896c519bdc26d60109ba5c2c88
sujikim <sujikim@student.42seoul.kr>
2023-08-30 06:31:03 +0900

    fix: 헤더 클릭시 목록으로 돌아가도록 링크 추가

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,12 +1,15 @@
 import { styled } from 'styled-components';
 import { OWNER, REPOSITORY } from 'configs/octokit';
 import { capitalize } from 'utils/format';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <HeaderCenter>
-      <StyledSpan>{capitalize(OWNER)}</StyledSpan>
-      <StyledSpan>{capitalize(REPOSITORY)}</StyledSpan>
+      <StyledLink to="/">
+        <StyledSpan>{capitalize(OWNER)}</StyledSpan>
+        <StyledSpan>{capitalize(REPOSITORY)}</StyledSpan>
+      </StyledLink>
     </HeaderCenter>
   );
 };
@@ -14,15 +17,22 @@ const Header = () => {
 export default Header;
 
 const HeaderCenter = styled.header`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   height: 5rem;
   background-color: white;
   box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
   flex-direction: row;
   position: sticky;
   top: 0;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const StyledSpan = styled.span`
