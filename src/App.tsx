08901003327aa64b7ec5b1fac104c320bd55eae9
sujikim <sujikim@student.42seoul.kr>
2023-08-29 23:26:29 +0900

    chore: 불필요한 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>CICD 구현 완료</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App">app</div>;
+};
 
 export default App;
